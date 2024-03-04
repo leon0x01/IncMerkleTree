cdc9f42fb39de491bc9e0876195caa359ccf8501
leon0x01 <ushrestha358@gmail.com>
2024-03-04 16:56:17 +0545

    feat: added necessary field for IncrementalMerkleTree

--- src/incremental_tree.rs
+++ src/incremental_tree.rs
@@ -10,3 +10,21 @@ pub enum IncrementalMerkleTreeError {
     /// Index out of bound.
     IndexOutOfBounds,
 }
+
+/// [IncrementalMerketTree] is an append-only merkle tree of 
+/// generic height, using `keccak256` as the hash function
+
+pub struct IncrementalMerkleTree<const HEIGHT: usize>{
+    /// The zero hashes
+    zero_hashes: [B256; HEIGHT],
+    /// The active branch of the tree, used to calculate the root hash 
+    active_branch: [B256; HEIGHT], 
+    /// The number of leaves that have been added to the tree
+    size: usize,
+    /// The intermediate cache for the tree, indexed by `generalized_index + 1`. The intermediates are
+    /// only valid if `cache_valid` is true.
+    intermediates: Vec<B256>,
+    /// Signals whether the intermediate cache is valid. Cache Validation is global, and all levels above 
+    /// the leaves will be recomputed during proof generation if it is invalid.
+    cache_valid: bool,
+}
